ffdb36da3819d41878dc63b2d5ecc66cca52c343
andrey-krav <andrey2003030@gmail.com>
2021-11-23 17:02:20 +0500

    modif

--- search.js
+++ search.js
@@ -1,14 +1,68 @@
 const { substringSearch } = require('./substringSearch.js');
 let fs = require('fs');
-
 let arg = process.argv;
 
-fs.readFile(arg[3], (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
+let data;
+try {
+	data = fs.readFileSync(arg[3]);
+} catch (err) {
+	console.err(err);
+}
+data = data.toString();
+
+let substring;
+try {
+	substring = fs.readFileSync(arg[2]);
+} catch (err) {
+	console.err(err);
+}
+substring = substring.toString();
+if (substring.charAt(substring.length - 1) == '\n')
+	substring = substring.substring(0, substring.length - 1);
+
+console.log(`Search "${substring}"(${arg[2]}) in "${arg[3]}"`)
+let result = data.substringSearch(substring.toString());
+if (result[0] == -1)
+	console.log('MATCH: 0')
+else {
+	console.log(`MATCH: ${result.length}`)
+	printResult(result);
+}
+mesureTime(data, substring);
+
+function mesureTime(input, substr) {
+	let result;
+	let data1;
+	let data2;
+	console.log('-'.repeat('18') + 'Statistics' + '-'.repeat('18'))
+	data1 = (new Date()).valueOf();
+	result = input.substringSearch(substr, 'RabinKarp', 'test');
+	data2 = (new Date()).valueOf();
+	console.log(`Algorythm Rabin-Karp:      ${data2-data1} ms, (${result} collisions)`);
+	
+	data1 = (new Date()).valueOf();
+	result = input.substringSearch(substr, 'goodHash', 'test');
+	data2 = (new Date()).valueOf();
+	console.log(`Good Hash(Sum of squares): ${data2-data1} ms, (${result} collisions)`);
+	
+	data1 = (new Date()).valueOf();
+	result = input.substringSearch(substr, 'bruteForce');
+	data2 = (new Date()).valueOf();
+	console.log(`Brute Force:               ${data2-data1} ms`);
+}
+
+function printResult(array) {
+	let digits = array[array.length - 1].toString().length;
+	const count = 10;
+	let str = '';
+	for (let i = 0; i < array.length; i++) {
+		let digitsCount = array[i].toString().length;
+		str += ' '.repeat(digits - digitsCount) + array[i] + ' '
+		if ( (i + 1) % 10 == 0) {
+			console.log(str);
+			str = '';
+		}
 	}
-	data = data.toString();
-	let result = data.substringSearch(arg[2].toString());
-	console.log(result.join(' '));
-});
+	if (str != '')
+		console.log(str);	
+} 
--- substringSearch.js
+++ substringSearch.js
@@ -1,29 +1,99 @@
-String.prototype.substringSearch = function (substr) {	
+String.prototype.substringSearch = function (substr, parametr = 'RabinKarp', test = 'N') {	
 	let result = new Array();
-	const substrHash = hash(substr);
-	let segmentHash = hash(this.substring(0, substr.length));
-	
-	let j;
-	for(let i = 0; i < this.length - substr.length + 1; i++) {
-		if (substrHash == segmentHash) {
-			j = 0;
-			while (j < substr.length && this[i + j] == substr[j])
-				j++;
-			if (j == substr.length)
-				result.push(i);
-		}
-		segmentHash = (segmentHash - this.charCodeAt(i) * Math.pow(2, substr.length - 1)) * 2 + this.charCodeAt(i + substr.length);
+	switch (parametr) {
+		case 'RabinKarp':
+			result = alogorythmRabinKarp(this, substr, test);
+			break;
+		case 'bruteForce':
+			result = bruteForce(this, substr);
+			break;
+		case 'goodHash':
+			result = goodHash(this, substr, test);
+			break;
+		default:
+			throw new Error(`Invalid comman: ${parametr}`)
 	}
-	
 	if (result.length != 0)
 		return result;
 	else
 		return [-1];	
 }
 
-function hash(input) {
-	let sumCode = 0;
-	for(let i = 0; i < input.length; i++)
-		sumCode += (input.charCodeAt(i) * Math.pow(2, input.length - i - 1));
-	return sumCode;
+function alogorythmRabinKarp(input, substr, test) {	
+	let collisionCount = 0;
+	let result = new Array();
+	const substrHash = hash(substr);
+	let segmentHash = hash(input.substring(0, substr.length));
+	let j = 0;
+	for(let i = 0; i < input.length - substr.length + 1; i++) {
+		if (substrHash == segmentHash) {
+			j = 0;
+			while (j < substr.length && input[i + j] == substr[j])
+				j++;
+			if (j == substr.length)
+				result.push(i);
+			else
+				collisionCount += 1;
+		}
+		segmentHash = (segmentHash - input.charCodeAt(i) * Math.pow(2, substr.length - 1)) * 2 + input.charCodeAt(i + substr.length);
+	}
+	if (test != 'N')
+		return collisionCount;
+	else 
+		return result;
+	
+	function hash(input) {
+		let sumCode = 0;
+		for(let i = 0; i < input.length; i++)
+			sumCode += (input.charCodeAt(i) * Math.pow(2, input.length - i - 1));
+		return sumCode;
+	}
 }
+
+function bruteForce(input, substr) {	
+	let result = new Array();
+	let j = 0;
+	for(let i = 0; i < input.length - substr.length + 1; i++) {
+		j = 0;
+		while (j < substr.length && input[i + j] == substr[j])
+			j++;
+		if (j == substr.length)
+			result.push(i);
+	}
+	return result;
+}
+
+function goodHash(input, substr, test) {	
+	let collisionCount = 0;
+	let result = new Array();
+	const substrHash = hash(substr);
+	let segmentHash = hash(input.substring(0, substr.length));
+	let j = 0;
+	for(let i = 0; i < input.length - substr.length + 1; i++) {
+		if (substrHash == segmentHash) {
+			j = 0;
+			while (j < substr.length && input[i + j] == substr[j])
+				j++;
+			if (j == substr.length)
+				result.push(i);
+			else
+				collisionCount += 1;
+		}
+		segmentHash -= (input.charCodeAt(i) * input.charCodeAt(i));
+		segmentHash += (input.charCodeAt(i + substr.length) * input.charCodeAt(i + substr.length));
+	}
+	
+	if (test != 'N')
+		return collisionCount;
+	else 
+		return result;
+	
+	function hash(input) {
+		let sumCode = 0;
+		for(let i = 0; i < input.length; i++)
+			sumCode += (input.charCodeAt(i) * input.charCodeAt(i))
+		return sumCode;
+	}
+}
+
+
